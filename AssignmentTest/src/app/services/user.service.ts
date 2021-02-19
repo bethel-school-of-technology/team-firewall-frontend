ab791e993700716696bb3678f9ecad6b1071288f
Zachary Mead <zacharymead11@gmail.com>
2021-02-18 21:17:04 -0500

    adding my code

--- AssignmentTest/src/app/services/user.service.ts
+++ AssignmentTest/src/app/services/user.service.ts
@@ -13,6 +13,15 @@ export class UserService {
   serverURL: string = 'http://localhost:3000'
   
   signupBankers(newbanker: Banker, newBank: Bank): Observable<any>{
-    return this.myHttp.post(this.serverURL + "/signup/banker", {newbanker, newBank});
+    var request = {
+      firstName: newbanker.FirstName,
+      lastName: newbanker.LastName,
+      email: newbanker.Email,
+      password: newbanker.Password,
+      bankName: newBank.Name
+    }; 
+    return this.myHttp.post(this.serverURL + "/signup/banker", request);
   }
+
+  
 }
