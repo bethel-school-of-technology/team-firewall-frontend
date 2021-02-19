7719946bcd2745843409eb1d029066e1541c7bf8
D <izzydman03@icloud.com>
2021-02-18 18:39:00 -0800

    adding SignupBkr and Bkrlogin

--- AssignmentTest/src/app/components/bkrlogin/bkrlogin.component.ts
+++ AssignmentTest/src/app/components/bkrlogin/bkrlogin.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Banker } from 'src/app/models/banker';
+import { UserService } from 'src/app/services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-bkrlogin',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bkrlogin.component.css']
 })
 export class BkrloginComponent implements OnInit {
+  bankerInfo: Banker = new Banker();
+ 
 
-  constructor() { }
+  constructor(private UserService: UserService) { }
 
   ngOnInit(): void {
+  }
+
+  login() {
+    console.log(this.bankerInfo);
+    this.UserService.loginBanker(this.bankerInfo.Email, this.bankerInfo.Password).subscribe(myResponseObject => {
+      console.log(myResponseObject);
+    })
+
   }
 
 }
