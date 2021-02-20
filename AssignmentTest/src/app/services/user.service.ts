2789c885cf2567fa40b8294429190d9b15fc5636
zillawyo <zillawyo@users.noreply.github.com>
2021-02-19 19:21:52 -0800

    started nav

--- AssignmentTest/src/app/services/user.service.ts
+++ AssignmentTest/src/app/services/user.service.ts
@@ -15,6 +15,17 @@ export class UserService {
   serverURL: string = 'http://localhost:3000'
   
   signupBankers(newbanker: Banker, newBank: Bank): Observable<any>{
-    return this.myHttp.post(this.serverURL + "/signup/banker", {newbanker, newBank});
+    var request = {
+      firstName: newbanker.FirstName,
+      lastName: newbanker.LastName,
+      email: newbanker.Email,
+      password: newbanker.Password,
+      bankName: newBank.Name
+    };
+    return this.myHttp.post(this.serverURL + "/signup/banker", request);
+  }
+
+  loginBanker(email: string, password: string): Observable<any>{
+    return this.myHttp.post(this.serverURL+"/login/Banker", {email,password})
   }
 }
