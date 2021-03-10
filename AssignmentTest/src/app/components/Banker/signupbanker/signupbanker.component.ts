bc109bac3f9857d21e21de3ab7bd70df077fa7b2
Zachary Mead <zacharymead@Zacharys-MacBook-Pro.local>
2021-03-10 15:40:03 -0500

    adding regex

--- AssignmentTest/src/app/components/Banker/signupbanker/signupbanker.component.ts
+++ AssignmentTest/src/app/components/Banker/signupbanker/signupbanker.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Bank } from 'src/app/models/bank';
 import { Banker } from 'src/app/models/banker';
@@ -12,10 +13,14 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./signupbanker.component.css']
 })
 export class SignupbankerComponent implements OnInit {
-  
+
+  pwdPattern = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$";
+  emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
   newBank: Bank = new Bank();
   newBanker: Banker = new Banker();
   banks: Bank[] = [];
+
+ 
 
   constructor(private UserService: UserService, private myRouter: Router) { }
 
@@ -28,12 +33,14 @@ export class SignupbankerComponent implements OnInit {
       });
   }
 
+  
+
   signupBanker() {
     console.log(this.newBanker, this.newBank);
     this.UserService.signupBankers(this.newBanker, this.newBank).subscribe(myResponseObject => {
       console.log(myResponseObject);
       if(myResponseObject.status === 200) {
-        window.alert(myResponseObject.messsage);
+        window.alert(myResponseObject.message);
         this.myRouter.navigate(["login/banker"]);
       } else {
         window.alert(myResponseObject.message);
@@ -41,5 +48,6 @@ export class SignupbankerComponent implements OnInit {
     });
   }
 
+  
   
 }
